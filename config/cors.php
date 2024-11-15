76c278b7d55c0cf1045b916fd3d11661d89bd8fe
dzackyluc <dzackylucifer12345@gmail.com>
2024-11-15 16:38:57 +0700

    update: refine CORS configuration to specify allowed methods and origins

--- config/cors.php
+++ config/cors.php
@@ -17,9 +17,9 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
 
-    'allowed_origins' => ['*', 'https://freshyfishapi.ydns.eu', 'http://localhost:3000'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
